2c71f1e6dabf8fb6d95daddf0579b8a30ff416e6
Chau Palma <chaupalma81@gmail.com>
2026-01-30 19:21:31 -0600

    Update

--- src/datafun_03_analytics/app_chaupalma.py
+++ src/datafun_03_analytics/app_chaupalma.py
@@ -1,9 +1,9 @@
-"""app_yourname.py - Project script.
+"""app_chaupalma.py - Project script.
 
-TODO: Replace "yourname" in the filename with your actual name or alias.
+: Replace "yourname" in the filename with your actual name or alias.
 
-TODO: Read the examples carefully. Choose your data source of one of the provided types.
-TODO: Create and implement a new Python file (module) in this folder following the associated example.
+: Read the examples carefully. Choose your data source of one of the provided types.
+: Create and implement a new Python file (module) in this folder following the associated example.
 TODO: Your module should have:
 - an appropriate name like yourname_type_pipeline.py (e.g., smith_csv_pipeline.py)
 - start with a docstring similar to the examples
@@ -14,8 +14,8 @@ TODO: Your module should have:
 - define a run_pipeline() function that calls E, T, L, and adds a new output file to data/processed/.
 TODO: Import and call your new module run_pipeline function in this script.
 
-Author: Your Name or Alias
-Date: 2026-01
+Author: Chau Palma
+Date: 2026-01-28
 
 Practice key Python skills:
 - pathlib for cross-platform paths
@@ -45,7 +45,7 @@ from datafun_toolkit.logger import get_logger, log_header
 # === IMPORT LOCAL MODULE FUNCTIONS ===
 # REQ: imports from other modules in this project must use full package path
 # TODO: create and import your own data pipeline module here. See the example code.
-
+from datafun_03_analytics.chaupalma_csv_pipeline import run_pipeline
 
 # === CONFIGURE LOGGER ONCE PER MODULE ===
 
@@ -67,6 +67,7 @@ def main() -> None:
     LOG.info("START main()")
 
     # TODO: call your imported data pipeline that reads from data/raw and writes to data/processed.
+    run_pipeline(raw_dir=RAW_DIR, processed_dir=PROCESSED_DIR, logger=LOG)
 
     LOG.info("END main()")
 
